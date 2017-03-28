e72ff4632d3ff48531435d52510b0f56dc2995b9
unknown <razheem@libero.it>
2017-03-28 12:12:55 +0200

    public and yellow page phonebook

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var p = require('path');
+const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const endOfLine = require('os').EOL;
-var sqlite = require('sqlite-sync');
+const sqlite = require('sqlite-sync');
 const readline = require('readline');
 
 const db_name = "phonebook_db.sql";
+
+const WEB_SERVER_PORT = 3000;
 
 const rl = readline.createInterface({
   input: fs.createReadStream(db_name)
@@ -13,7 +15,7 @@ const rl = readline.createInterface({
 
 //creo il db
 sqlite.connect(':memory:');
-sqlite.run("CREATE TABLE contacts (id INT, name TEXT, surname TEXT, number TEXT)");	
+sqlite.run("CREATE TABLE contacts (id INT, name TEXT, surname TEXT, number TEXT, office TEXT, mobile TEXT)");	
 console.log("DB created [OK]");
   
 idRow = 1;
@@ -29,11 +31,9 @@ rl.on('close', function(close) {
 });
 
 var app = express();
-var secureDir = p.join(__dirname, "public");
-
 
 app.disable('x-powered-by');
-app.use('/backend', express.static(secureDir));
+app.use('/backend', express.static( path.join(__dirname, "public")));
 
 app.get('/', function (req, res) {
 	
@@ -48,39 +48,42 @@ app.get('/', function (req, res) {
 		let totalContacts = sqlite.run("SELECT id from contacts");
 				
 		let tot = (count < results.length) ? count : results.length;
-		
-		let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"+endOfLine;
-		xml += "<list response=\"get_list\" type=\"pb\" total=\""+totalContacts.length+"\" first=\""+first+"\" last=\""+(Number(count)+Number(first)-1)+"\">"+endOfLine;
-		let xmlBody = "";
-		
-		for(let i=0;i<tot; i++){
-			xmlBody += "<entry id=\""+(i+1)+"\">"+endOfLine;
-			xmlBody += "<fn>"+results[i].name+"</fn>"+endOfLine;
-			xmlBody += "<ln>"+results[i].surname+"</ln>"+endOfLine;
-			xmlBody += "<hm>"+results[i].number+"</hm>"+endOfLine;
-			xmlBody += "</entry>"+endOfLine;
-		}
-		xmlBody += "</list>";
-		
-	    xml += xmlBody;
+
+		let xml = getXMLPhonebook(results, type, count, first, totalContacts.length, tot);
 		//DEBUG - da rimuovere
 		//console.log(xml);
 		res.send(xml);
-	}
-	else{
-		res.status(500);
-		res.send('error');
+	}else{
+			res.status(500);
+			res.send('error');		
 	}
 });
 
-app.listen(3000, function () { });
+app.listen(WEB_SERVER_PORT, function () { });
 
-console.log("Server started [OK]");
+console.log("Server started on port "+WEB_SERVER_PORT+" [OK]");
 console.log("Press CTRL+C to exit");
 
 process.on('SIGINT', function() {
-    console.log("Closed DB [OK]");
+	console.log("Closed DB [OK]");
 	sqlite.close();
 	console.log("Exting");	
-    process.exit();
+	process.exit();
 });
+
+function getXMLPhonebook(r, t, c, f, ctot, tot){
+	let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"+endOfLine;
+	xml += "<list response=\"get_list\" type=\""+t+"\" total=\""+ctot+"\" first=\""+f+"\" last=\""+(Number(c)+Number(f)-1)+"\">"+endOfLine;
+	let xmlBody = "";
+		
+	for(let i=0;i<tot; i++){
+		xmlBody += "<entry id=\""+(i+1)+"\">"+endOfLine;
+		xmlBody += "<fn>"+r[i].name+"</fn>"+endOfLine;
+		xmlBody += "<ln>"+r[i].surname+"</ln>"+endOfLine;
+		xmlBody += "<hm>"+r[i].number+"</hm>"+endOfLine;
+		xmlBody += "</entry>"+endOfLine;
+	}
+	xmlBody += "</list>";	
+	xml += xmlBody;
+	return xml;
+}
